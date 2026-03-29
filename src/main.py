79f6b9dc3fb2ba8a725b24760213d850685b0c46
Tuan ITF Dev <tuanitfdev@gmail.com>
2026-03-29 19:42:58 +0700

    fix bug to load model

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import stable_whisper as whisper
 
 model = whisper.load_faster_whisper(
+    # "deepdml/faster-whisper-large-v3-turbo-ct2", 
     "large-v3", 
     device="cuda", 
     compute_type="float16",
-    device_index=[0]
+    device_index=[0],
+    download_root="data/models",
 )
 
 print("Model loaded successfully")
