3d24bfd50a22d1c35157b374e949cae7088a1d4a
TuanITF Le <tuanitfdev@gmail.com>
2026-04-03 04:14:08 +0000

    update more cache data and update ignore file with dockerignore, gitignore

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM alpine:latest
-COPY data/models /models
+COPY data/models /data/models
+COPY data/cacheTorchHub /data/cacheTorchHub
+COPY data/whl /data/whl
 CMD ["tail", "-f", "/dev/null"]
--- setupAtHost.sh
+++ setupAtHost.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 uv run python src/main.py
 wget -P data/whl/ https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.9cxx11abiTRUE-cp312-cp312-linux_x86_64.whl
+mkdir -p data/cacheTorchHub && curl -L https://github.com/snakers4/silero-vad/tarball/master -o data/cacheTorchHub/snakers4_silero-vad_master.tar.gz
 
